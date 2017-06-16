ced1e54c5e7def69a2446b642cab71809aff636b
Ken Gribble <gribble@cs.ucdavis.edu>
2017-06-16 11:25:33 -0700

    prep for testing part 4

--- manifests/config.pp
+++ manifests/config.pp
@@ -4,20 +4,20 @@ class gitolitepuppet::config inherits gitolitepuppet {
 		owner => 'gitolite3',
 		group => 'gitolite3',
 		mode => 0755,
-		content => template(gitolitepuppet/puppet-update.erb),
+		content => template('gitolitepuppet/puppet-update.erb'),
 	}
 	file { '/var/repos/local/hooks/repo-specific/puppet-post-receive':
 		ensure => file,
 		owner => 'gitolite3',
 		group => 'gitolite3',
 		mode => 0755,
-		content => template(gitolitepuppet/puppet-post-receive.erb),
+		content => template('gitolitepuppet/puppet-post-receive.erb'),
 	}
 	file { '/var/repos/.gitolite.rc':
 		ensure => file,
 		owner => 'gitolite3',
 		group => 'gitolite3',
 		mode => 0600,
-		content => template(gitolitepuppet/gitolite.rc.erb),
+		content => template('gitolitepuppet/gitolite.rc.erb'),
 	}
 }
