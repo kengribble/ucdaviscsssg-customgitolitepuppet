f74873d8c3549c9ea2a0947b6a21fe383ce47392
Ken Gribble <gribble@cs.ucdavis.edu>
2017-06-16 11:02:44 -0700

    making configuration

---
+++ config.pp
@@ -0,0 +1,23 @@
+class gitolitepuppet::config.pp inherits gitolitepuppet {
+	file { '/var/repos/local/VREF/puppet-update':
+		ensure => file,
+		owner => 'gitolite3',
+		group => 'gitolite3',
+		mode => 0755,
+		content => template(gitolitepuppet/puppet-update.erb),
+	}
+	file { '/var/repos/local/hooks/repo-specific/puppet-post-receive':
+		ensure => file,
+		owner => 'gitolite3',
+		group => 'gitolite3',
+		mode => 0755,
+		content => template(gitolitepuppet/puppet-post-receive.erb),
+	}
+	file { '/var/repos/.gitolite.rc':
+		ensure => file,
+		owner => 'gitolite3',
+		group => 'gitolite3',
+		mode => 0600,
+		content => template(gitolitepuppet/gitolite.rc.erb),
+	}
+}
