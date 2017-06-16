6d845d81f594d5096de74c8f2f86341fe30f149f
Ken Gribble <gribble@cs.ucdavis.edu>
2017-06-16 15:41:37 -0700

    prep for testing part 4

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,10 +1,18 @@
 class gitolitepuppet::config inherits gitolitepuppet {
+	file { '/var/repos/local/VREF':
+		ensure => directory,
+		recurse => true,
+	}
 	file { '/var/repos/local/VREF/puppet-update':
 		ensure => file,
 		owner => 'gitolite3',
 		group => 'gitolite3',
 		mode => '0755',
 		content => epp('gitolitepuppet/puppet-update.epp'),
+	}
+	file { '/var/repos/local/hooks/repo-specific/puppet-post-receive':
+		ensure => directory,
+		recurse => true,
 	}
 	file { '/var/repos/local/hooks/repo-specific/puppet-post-receive':
 		ensure => file,
